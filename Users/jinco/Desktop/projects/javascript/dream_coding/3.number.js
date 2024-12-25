802fcb68ab5760bda80bf3ebcdd1d30f64373ee5
lhjwork <jin.come.up.business@gmail.com>
2024-12-25 14:44:38 +0900

    3.number.js 숫자타입

---
+++ Users/jinco/Desktop/projects/javascript/dream_coding/3.number.js
@@ -0,0 +1,20 @@
+let integer = 123; // 음수
+let double = 12.34; // 실수
+let negative = -12.34; // 음수
+
+console.log(integer);
+console.log(double);
+console.log(negative);
+
+let binary = 0b11111111; // 255: 2진수
+let octal = 0o377; // 255: 8진수
+let hex = 0xff; // 255: 16진수
+
+console.log(binary);
+console.log(octal);
+console.log(hex);
+
+console.log(0 / 123); // 0
+console.log(123 / 0); // Infinity
+console.log(123 / -0); // -Infinity
+console.log(123 / "hello"); // NaN
